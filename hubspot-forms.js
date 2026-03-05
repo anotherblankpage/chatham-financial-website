22fa8bbef9b92f404c17c1c231d2acb1dda78509
Kevin Quach <kevinquach101@gmail.com>
2026-03-05 14:26:46 -0800

    styling hubspot theme

---
+++ hubspot-forms.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        surface: {
+          page:  '#0d0d0d',
+          card:  '#161616',
+          input: '#1c1c1c',
+          hover: '#222222',
+        },
+        border: {
+          subtle:  '#2a2a2a',
+          checked: '#4a4a4a',
+        },
+      },
+    },
+  },
+};
